cfb7530f6463259ae999e1f5afd913427e632458
Vo <huynhvo47@gmail.com>
2024-04-11 12:40:03 +0700

    updated: added import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,35 @@ import { UserModule } from './user/user.module';
 import { ClassModule } from './class/class.module';
 import { PostModule } from './post/post.module';
 import { AwsModule } from './aws/aws.module';
-import { FileModule } from './file/file.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MailModule } from './mail/mail.module';
+import { SocketModule } from './socket/socket.module';
+import { CronjobModule } from './cronjob/cronjob.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { join } from 'path';
+import { NotificationModule } from './notification/notification.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
     imports: [
+        MailerModule.forRoot({
+            transport: {
+                service: 'Gmail',
+                port: 465,
+                auth: {
+                    type: 'OAuth2',
+                    clientSecret: process.env.MAIL_CLIENT_SECRET,
+                    user: process.env.MAIL_USER,
+                    clientId: process.env.MAIL_CLIENT_ID,
+                    expires: 3599,
+                    accessToken: process.env.MAIL_ACCESS_TOKEN,
+                    refreshToken: process.env.MAIL_REFRESH_TOKEN,
+                },
+            },
+            defaults: {
+                from: 'No reply from SupEdu',
+            },
+        }),
         AuthModule,
         PrismaModule,
         ConfigModule.forRoot({
@@ -19,7 +44,12 @@ import { FileModule } from './file/file.module';
         ClassModule,
         PostModule,
         AwsModule,
-        FileModule,
+        MailModule,
+        SocketModule,
+        ScheduleModule.forRoot(),
+        CronjobModule,
+        NotificationModule,
+        ChatModule,
     ],
 })
 export class AppModule {}
