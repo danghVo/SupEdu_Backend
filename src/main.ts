0adc1852f03730153d38bc2ecd61c293362ac041
Vo <huynhvo47@gmail.com>
2024-04-11 12:47:40 +0700

    updated: changed port from 3000 to 4000, added useGlobalFilters AllException, allow Cors

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './interceptor/response.interceptor';
+import { AllExceptionFilter } from './exception/AllExceptionFilter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalInterceptors(new ResponseInterceptor());
+    const httpAdapterHost = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new AllExceptionFilter(httpAdapterHost));
+    app.enableCors();
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true, // elimating unwanted fields
         }),
     );
-    await app.listen(3000);
+    await app.listen(4000);
 }
 bootstrap();
